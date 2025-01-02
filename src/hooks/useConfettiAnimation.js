cfabc8e23244553232972f2621e2432835c0e9c6
mtalha77 <mtalha77@gmail.com>
2025-01-03 01:51:24 +0500

    Update useConfettiAnimation.js

--- src/hooks/useConfettiAnimation.js
+++ src/hooks/useConfettiAnimation.js
@@ -1,24 +1,25 @@
-'use client'
 import { useEffect, useState } from "react";
 
 export const useConfettiAnimation = () => {
   const [isAnimating, setIsAnimating] = useState(false);
   const [windowDimensions, setWindowDimensions] = useState({
-    width: typeof window !== "undefined" ? window.innerWidth : 0,
-    height: typeof window !== "undefined" ? window.innerHeight : 0,
+    width: 0,
+    height: 0,
   });
 
   useEffect(() => {
-    const handleResize = () => {
-      setWindowDimensions({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        setWindowDimensions({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
   }, []);
 
   const startAnimation = () => {
